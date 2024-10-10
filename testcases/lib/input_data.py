16d8d2037add7edbe48e813fa0c51a985a8a9c76
Mrinal Raj <mrinalraj2809@gmail.com>
2024-10-10 22:52:43 +0530

    Added Input data file

---
+++ testcases/lib/input_data.py
@@ -0,0 +1,32 @@
+a_id_for_amazon_app = "Predicted app: Amazon"
+id_for_crash = "android:id/aerr_wait"
+id_for_account = "in.amazon.mShop.android.shopping:id/bottom_tab_button_icon"
+id_for_home_icon = "in.amazon.mShop.android.shopping:id/bottom_tab_label"
+xpath_for_home_icon = "(//android.widget.ImageView[@resource-id='in.amazon.mShop.android.shopping:id/bottom_tab_button_icon'])[1]"
+xpath_for_mx_player = "//android.widget.TextView[@text='MX Player']"
+xpath_for_magnifier_icon = "//android.widget.TextView[@text='Search']"
+xpath_for_search_box = "//android.widget.EditText"
+series_name = "Highway Love"
+xpath_for_trending_series = "//android.webkit.WebView[@text='Amazon miniTV - Watch Free Web Series, Movies, Short Films & K-Dramas Online']"
+xpath_for_highway_series = "//android.view.View[@text='Highway Love Title']"
+xpath_for_searched_series = "//android.widget.Button[@text='share Highway Love - Season 2 Comedy • Romance 6 Episodes share Highway Love - Season 1 Romance • Comedy 4 Episodes share Highway Love (Telugu) - Season1 Romance • Comedy 4 Episodes share Highway Love (Tamil) - Season1 Romance •']"
+xpath_for_series_expand = "(//android.widget.Image[contains(@text,'expand')])[2]"
+xpath_for_seasons = "(//android.widget.ListView[@text='Please select a value'])[2]//android.view.View[contains(@text,'Season ')]"
+xpath_for_season = lambda x: f"(//android.view.View[(@text='{x}')])[2]"
+xpath_for_series_expand2 = "(//android.widget.Image[@text='expand-icon'])[2]"
+xpath_for_all_episode_element = "//android.widget.Button[contains(@text, 'E1 ')]"
+x_loc_of_magnifier_icon = 316
+y_loc_of_magnifier_icon = 2272
+x_loc_of_search_box = 463
+y_loc_of_search_box = 330
+WAIT_FOR_1_SEC = 1
+WAIT_FOR_2_SEC = 2
+WAIT_FOR_3_SEC = 3
+WAIT_FOR_4_SEC = 4
+WAIT_FOR_5_SEC = 5
+WAIT_FOR_6_SEC = 6
+WAIT_FOR_7_SEC = 7
+WAIT_FOR_10_SEC = 10
+WAIT_FOR_15_SEC= 15
+WAIT_FOR_20_SEC = 20
+WAIT_FOR_25_SEC = 25
